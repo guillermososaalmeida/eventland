e10e4e57e4710f4454df3ee27f921046c58004ed
guillebags <guillebags@gmail.com>
2023-09-05 13:51:08 +0200

    middleware de autorizar user y admin

--- Backend/src/middleware/authUser.middleware.js
+++ Backend/src/middleware/authUser.middleware.js
@@ -1,4 +1,4 @@
-/* //? Aquí haremos la autentificación de usuarios y usuarios-Administradores
+//? Aquí haremos la autentificación de usuarios y usuarios-Administradores
 
 const User = require("../api/models/user.model");
 const { verifyToken } = require("../utils/token");
@@ -9,7 +9,7 @@ const isAuth = async (req, res, next) => {
   //le quitamos el prefijo Bearer al token
   const token = req.headers.authorization?.replace("Bearer ", "");
   if (!token) {
-    return next(new Error("Unauthorized"));
+    return next(new Error("You're not authorized ❌"));
   }
   try {
     const decoded = verifyToken(token, process.env.JWT_SECRET);
@@ -23,15 +23,14 @@ const isAuth = async (req, res, next) => {
 const isAuthAdmin = async (req, res, next) => {
   const token = req.headers.authorization?.replace("Bearer ", "");
   if (!token) {
-    return next(new Error("Unauthorized"));
+    return next(new Error("You're not authorized as Admin ❌"));
   }
 
   try {
     const decoded = verifyToken(token, process.env.JWT_SECRET);
-    console.log(decoded);
     req.user = await User.findById(decoded.id);
     if (req.user.rol !== "admin") {
-      return next(new Error("Unauthorized, not admin"));
+      return next(new Error("You need to be Admin for this ❌"));
     }
     next();
   } catch (error) {
@@ -43,4 +42,3 @@ module.exports = {
   isAuth,
   isAuthAdmin,
 };
- */
--- README.md
+++ README.md
@@ -13,6 +13,9 @@ luego instalamos:
 - npm i cloudinary multer multer-storage-cloudinary (gestión de archivos multimedia, almacenarlos en una nube)
 - npm i -D eslint eslint-config-prettier eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-prettier eslint-plugin-react eslint-plugin-simple-import-sort pre-commit prettier (control de errores, formateo código)
 - npm i cors (CORS (Cross-Origin Resource Sharing) es un mecanismo de seguridad que permite o niega el acceso a recursos de un dominio a una aplicación en otro dominio. En el contexto de una API en Node.js, CORS es una técnica para controlar qué aplicaciones pueden hacer solicitudes a la API.)
+- npm i jsonwebtoken
+- npm i bcrypt
+- npm i validator
 
 cambiamos en el package json los scripts a:
 
