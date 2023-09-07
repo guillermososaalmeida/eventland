80d9207021b09f184314b9774c9932d451ef3241
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-09-07 10:18:09 +0200

    rutas con auth en events

--- Backend/src/api/routes/Event.routes.js
+++ Backend/src/api/routes/Event.routes.js
@@ -8,13 +8,26 @@ const {
   postEvent,
   updateEvent,
 } = require("../controllers/Event.controller");
+const {
+  isAuthOrganization,
+} = require("../../middleware/authOrganization.middleware");
 
 const EventRoutes = express.Router();
 
-EventRoutes.post("/", uploadEvent.single("image"), postEvent); //luego poner la authOrganization
+EventRoutes.post(
+  "/",
+  uploadEvent.single("image"),
+  [isAuthOrganization],
+  postEvent,
+); //luego poner la authOrganization
 EventRoutes.get("/:id", getById);
 EventRoutes.get("/getbyname/:name", getByName);
 EventRoutes.get("/get/all", getAllEvents);
-EventRoutes.patch("/update/:id", uploadEvent.single("image"), updateEvent);
+EventRoutes.patch(
+  "/update/:id",
+  uploadEvent.single("image"),
+  [isAuthOrganization],
+  updateEvent,
+);
 
 module.exports = EventRoutes;
