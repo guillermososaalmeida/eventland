88a4a62bb89f491fb5729eb02f8cd795f6f21dac
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-09-08 16:00:18 +0200

    reorganizado middleware Organization y rutas

--- Backend/src/middleware/authOrganization.middleware.js
+++ Backend/src/middleware/authOrganization.middleware.js
@@ -22,6 +22,26 @@ const isAuthOrganization = async (req, res, next) => {
         new Error("You need to be Admin or Organization for this ❌"),
       );
     }
+    next();
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const isAuthOrganizationDeleteOrUpdate = async (req, res, next) => {
+  const token = req.headers.authorization?.replace("Bearer ", "");
+  if (!token) {
+    return next(new Error("You're not an organization ❌"));
+  }
+  try {
+    const decoded = verifyToken(token, process.env.JWT_SECRET);
+    req.user = await User?.findById(decoded.id);
+    req.organization = await Organization?.findById(decoded.id);
+    if (!req.organization && req.user?.role !== "admin") {
+      return next(
+        new Error("You need to be Admin or Organization for this ❌"),
+      );
+    }
     if (req.organization) {
       const eventId = req.params.id;
       const event = await Event.findById(eventId);
@@ -36,5 +56,6 @@ const isAuthOrganization = async (req, res, next) => {
 };
 
 module.exports = {
+  isAuthOrganizationDeleteOrUpdate,
   isAuthOrganization,
 };
