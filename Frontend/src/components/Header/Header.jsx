24eb727eb582fc88c70660bf515371020aa16b0d
guillebags <guillebags@gmail.com>
2023-09-11 15:39:10 +0200

    installs chakra y icons

---
+++ Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  display: flex;
+  align-items: center;
+  gap: 1em;
+  padding: 1em;
+`;
+
+export const Header = ({ children }) => {
+  return <HeaderStyled>{children}</HeaderStyled>;
+};
