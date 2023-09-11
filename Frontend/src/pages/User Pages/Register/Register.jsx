2de407b7e07ec9d6235236e06e1cf256ba14844e
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-09-11 15:43:33 +0200

    register a medias

--- Frontend/src/pages/User Pages/Register/Register.jsx
+++ Frontend/src/pages/User Pages/Register/Register.jsx
@@ -1,11 +1,60 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import { useAuth } from "../../../context/authContext";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { registerUser } from "../../../services/user.service";
+import { useRegisterError } from "../../../hooks/useRegisterError";
 
 export const Register = () => {
+  const { setUserComplete, bridgeData } = useAuth();
+  const {
+    register,
+    hanleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [res, setRes] = useState({});
+  const [send, setSend] = useState(false);
+  const [okRegister, setOkRegister] = useState(false);
+  const [gender, setGender] = useState(null);
+
+  const formSubmit = async (formData) => {
+    const inputFile = document.getElementById("file-upload").files;
+    if (inputFile.length !== 0) {
+      const customFormData = {
+        ...formData,
+        role: "cliente",
+        gender: gender,
+        image: inputFile[0],
+      };
+      setSend(true);
+      setRes(await registerUser(customFormData));
+      setSend(false);
+    } else {
+      const customFormData = {
+        ...formData,
+        role: "cliente",
+        gender: gender,
+      };
+      setSend(true);
+      setRes(await registerUser(customFormData));
+      setSend(false);
+    }
+  };
+  useEffect(() => {
+    useRegisterError(res, setOkRegister, setRes, setUserComplete);
+    if (res?.status === 200) bridgeData("USERCOMPLETE");
+  }, [res]);
+
+  if (okRegister) {
+    return <Navigate to="/check" />;
+  }
   return (
-    <div>
-      <Link to="/login">Login</Link>
-      <br />
-      <Link to="/check">Check Code</Link>
-    </div>
+    <></>
+
+    // <div>
+    //   <Link to="/login">Login</Link>
+    //   <br />
+    //   <Link to="/check">Check Code</Link>
+    // </div>
   );
 };
