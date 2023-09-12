34f6037f718675ab8f6c8c3052df5b752794150f
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-09-12 17:36:30 +0200

    poner contexto de org al app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,18 +3,21 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { AuthContextProvider } from "./context/authContext";
 import { Header } from "./components";
 import "./App.css";
+import { AuthOrgContextProvider } from "./context/authOrgContext";
 
 const App = () => {
   return (
     <>
-      <AuthContextProvider>
-        <ChakraProvider>
-          <Header />
-          <main>
-            <Outlet />
-          </main>
-        </ChakraProvider>
-      </AuthContextProvider>
+      <AuthOrgContextProvider>
+        <AuthContextProvider>
+          <ChakraProvider>
+            <Header />
+            <main>
+              <Outlet />
+            </main>
+          </ChakraProvider>
+        </AuthContextProvider>
+      </AuthOrgContextProvider>
     </>
   );
 };
