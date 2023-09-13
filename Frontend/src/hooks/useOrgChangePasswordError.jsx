5f353b83ca7cc312472db5b9b9b6c39837e3bd22
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-09-13 09:42:56 +0200

    hook errors useChangePasswordError

---
+++ Frontend/src/hooks/useOrgChangePasswordError.jsx
@@ -0,0 +1,64 @@
+import Swal from "sweetalert2/dist/sweetalert2.all.js";
+export const useOrgChangePasswordError = (res, setRes, setOrganization) => {
+  console.log("entro al custom hook 💚");
+  //!----------------- 200: updateOrganization: true,
+  if (res?.data?.updateOrganization?.toString() == "true") {
+    setOrganization(() => null);
+    localStorage.removeItem("organization");
+    setRes(() => ({}));
+    return Swal.fire({
+      icon: "success",
+      title: "Change password ok ✅",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
+  //!------------------200: updateOrganization: false,
+  if (res?.data?.updateOrganization?.toString() == "false") {
+    setRes(() => ({}));
+    return Swal.fire({
+      icon: "error",
+      title: "Interval server error ❎.",
+      text: "Please, try again",
+      showConfirmButton: false,
+      timer: 2500,
+    });
+  }
+
+  //! -----------------404: 'password dont match'
+  if (res?.response?.data?.includes("password dont match")) {
+    console.log("password ❌");
+    setRes(() => ({}));
+    return Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Old password don't match,  ❎ Try again, please",
+      showConfirmButton: false,
+      timer: 3000,
+    });
+  }
+
+  //! -----------------404: general
+  if (res?.response?.status == 404) {
+    setRes(() => ({}));
+    return Swal.fire({
+      icon: "error",
+      title: "Interval server error ❎.",
+      text: "Please, try again",
+      showConfirmButton: false,
+      timer: 3000,
+    });
+  }
+
+  //! -----------------500: interval server error
+  if (res?.response?.status == 500) {
+    setRes(() => ({}));
+    return Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Interval Server Error ❎!",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
+};
