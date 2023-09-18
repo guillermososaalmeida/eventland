4a64c8626738d579232ee13d10b919855cff3590
guillebags <guillebags@gmail.com>
2023-09-18 18:07:45 +0200

    styled component separado

---
+++ Frontend/src/components/Header/Header.element.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HeaderStyled = styled.header`
+  display: flex;
+  align-items: center;
+  gap: 1em;
+  padding: 1em;
+`;
--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,23 +1,20 @@
-import { Flex, Icon } from "@chakra-ui/react";
-import styled from "styled-components";
+import { Button, Flex, Icon } from "@chakra-ui/react";
 import { InputHeader } from "../Inputs/Input";
 import { MobileMenu } from "../Menu/MobileMenu";
 import useWidth from "../../hooks/useWidth";
 import { DesktopMenu } from "../Menu/DesktopMenu";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { useOrgAuth } from "../../context/authOrgContext";
 import { useAuth } from "../../context/authContext";
 import "./Header.css";
-
-const HeaderStyled = styled.header`
-  display: flex;
-  align-items: center;
-  gap: 1em;
-  padding: 1em;
-`;
+import { HeaderStyled } from "./Header.element";
+import { useState } from "react";
 
 export const Header = () => {
+  const [isOrg, setIsOrg] = useState(false);
+  const [isUser, setIsUser] = useState(false);
+  const navigate = useNavigate();
   const { organization, logoutOrg } = useOrgAuth();
   const { user, logout } = useAuth();
   const { width } = useWidth();
@@ -40,9 +37,23 @@ export const Header = () => {
       <Menu
         switchMode={
           pathname.includes("org") ? (
-            <Link onClick={logoutOrg}>Eventland para usuarios</Link>
+            <Button
+              onClick={() => {
+                setIsUser(() => true);
+                organization ? logoutOrg(isUser, setIsUser) : navigate("/");
+              }}
+            >
+              Eventland para usuarios
+            </Button>
           ) : (
-            <Link onClick={logout}>Crea tus propios eventos</Link>
+            <Button
+              onClick={() => {
+                setIsOrg(() => true);
+                user ? logout(isOrg, setIsOrg) : navigate("/org");
+              }}
+            >
+              Crea tus propios eventos
+            </Button>
           )
         }
         login={
