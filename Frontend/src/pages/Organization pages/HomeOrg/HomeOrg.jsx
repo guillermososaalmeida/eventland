13c8dfc17b33cd3c8628c364044d9a601dee906c
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-09-19 18:15:09 +0200

    boton create event

--- Frontend/src/pages/Organization pages/HomeOrg/HomeOrg.jsx
+++ Frontend/src/pages/Organization pages/HomeOrg/HomeOrg.jsx
@@ -1,16 +1,15 @@
 import { Link } from "react-router-dom";
 
 import { OrgNextEvents, OrgPastEvents } from "../../../components";
+import { Button } from "@chakra-ui/react";
 
 export const HomeOrg = () => {
   return (
     <>
       <div>
-        <Link to="/organizationprofile">Organization Profile</Link>
-        <br />
-        <Link to="/createeventorg">Create Event</Link>
-        <br />
-        <Link to="/eventdetailorg">Event Detail de Organización</Link>
+        <Link to="/createeventorg">
+          <Button>Create Event</Button>
+        </Link>
       </div>
       <OrgNextEvents />
       <OrgPastEvents />
