fbb4e04d26d37397401dfa6edc4af476cac53bd1
guillebags <guillebags@gmail.com>
2023-09-20 22:02:29 +0200

    color mode custom
    Co-authored-by: RafaGadSan <RafaGadSan@users.noreply.github.com>

--- Frontend/src/pages/Welcome/Welcome.jsx
+++ Frontend/src/pages/Welcome/Welcome.jsx
@@ -1,42 +1,48 @@
-import { Heading } from "@chakra-ui/react";
+import { Center, Heading, Stack } from "@chakra-ui/react";
 
 import "./Welcome.css";
 
 export const Welcome = () => {
+  const bg = ("#f6f3e0", "#173F4B");
+  const color = ("#173F4B", "#f6f3e0");
   return (
     <>
-      <div className="fullContainer">
-        <Heading w="15em" className="textoCabecera">
-          Todos los eventos al alcance de tu mano en Eventland
-        </Heading>
-
-        <div className="grid">
-          <img
-            className="image1"
-            src="https://res.cloudinary.com/dhr13yihn/image/upload/v1695223761/proyectoEventland/assets/OIG_ovz6jg.jpg"
-          />
-          <img
-            className="image2"
-            src="https://res.cloudinary.com/dhr13yihn/image/upload/v1695223741/proyectoEventland/assets/OIG_vtzvto.jpg"
-          />
-          <img
-            className="image3"
-            src="https://res.cloudinary.com/dhr13yihn/image/upload/v1695223064/proyectoEventland/assets/OIG_vv006f.jpg"
-          />
-          <img
-            className="image4"
-            src="https://res.cloudinary.com/dhr13yihn/image/upload/v1695223056/proyectoEventland/assets/OIG_exlai3.jpg"
-          />
-          <img
-            className="image5"
-            src="https://res.cloudinary.com/dhr13yihn/image/upload/v1695223045/proyectoEventland/assets/OIG_jjtect.jpg"
-          />
-          <img
-            className="image6"
-            src="https://res.cloudinary.com/dhr13yihn/image/upload/v1695226016/proyectoEventland/assets/OIG_lfkcw1.jpg"
-          />
+      <Center bg={bg} color={color}>
+        <div className="fullContainer">
+          <Stack gap="2em">
+            <Heading w="15em" id="textoCabecera" letterSpacing="1px">
+              Asiste a lo mejor, guarda lo inolvidable
+            </Heading>
+            <Heading id="subtextoCabecera">Eventland</Heading>
+          </Stack>
+          <div className="grid">
+            <img
+              className="image1"
+              src="https://res.cloudinary.com/dhr13yihn/image/upload/v1695223761/proyectoEventland/assets/OIG_ovz6jg.jpg"
+            />
+            <img
+              className="image2"
+              src="https://res.cloudinary.com/dhr13yihn/image/upload/v1695223741/proyectoEventland/assets/OIG_vtzvto.jpg"
+            />
+            <img
+              className="image3"
+              src="https://res.cloudinary.com/dhr13yihn/image/upload/v1695223064/proyectoEventland/assets/OIG_vv006f.jpg"
+            />
+            <img
+              className="image4"
+              src="https://res.cloudinary.com/dhr13yihn/image/upload/v1695223056/proyectoEventland/assets/OIG_exlai3.jpg"
+            />
+            <img
+              className="image5"
+              src="https://res.cloudinary.com/dhr13yihn/image/upload/v1695223045/proyectoEventland/assets/OIG_jjtect.jpg"
+            />
+            <img
+              className="image6"
+              src="https://res.cloudinary.com/dhr13yihn/image/upload/v1695226016/proyectoEventland/assets/OIG_lfkcw1.jpg"
+            />
+          </div>
         </div>
-      </div>
+      </Center>
     </>
   );
 };
